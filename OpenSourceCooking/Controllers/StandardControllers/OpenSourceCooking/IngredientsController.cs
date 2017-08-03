7ba6c4cf4566c362b77bc14268d46aabc00d7ed4
Jack Poole <Admin@OpenSourceCooking.com>
2017-08-02 20:39:22 -0700

    Cleanup

--- OpenSourceCooking/Controllers/StandardControllers/OpenSourceCooking/IngredientsController.cs
+++ OpenSourceCooking/Controllers/StandardControllers/OpenSourceCooking/IngredientsController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 using OpenSourceCooking.Models;
 
 namespace OpenSourceCooking.Controllers.StandardControllers
@@ -15,64 +11,13 @@ namespace OpenSourceCooking.Controllers.StandardControllers
     {
         private OpenSourceCookingEntities db = new OpenSourceCookingEntities();
 
-        // GET: Ingredients
-        public async Task<ActionResult> Index()
-        {
-            var ingredients = db.Ingredients.Include(i => i.AspNetUser).Include(i => i.CloudFile);
-            //This is to prevent img src breaking on nulls
-            foreach (Ingredient ingredient in ingredients)
-            {
-                if (ingredient.CloudFile == null) 
-                    ingredient.CloudFile = new CloudFile();
-            }
-            return View(await ingredients.ToListAsync());
-        }
-
-        // GET: Ingredients/Create
-        [Authorize]
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Ingredients/Create
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "IngredientName")] Ingredient ingredient, HttpPostedFileBase httpPostedFileBase)
-        {
-            string aspNetId = User.Identity.GetUserId();
-            DateTime UTCTimeNow = DateTime.UtcNow;
-            ingredient.CreateDate = UTCTimeNow;
-            ingredient.CreatorId = aspNetId;
-            if (ModelState.IsValid)
-            {
-                //Save Ingredient Cloud File
-                if (httpPostedFileBase != null)
-                {
-                    int UploadedCloudFileId = await AzureCloudStorageWrapper.UploadCloudFile(AzureCloudStorageWrapper.AzureBlobContainer.ingredientcloudfiles, httpPostedFileBase, aspNetId, "Ingredient-" + ingredient.IngredientName, true);
-                    if(UploadedCloudFileId == -1)//File size too big
-                    {
-                        ViewBag.FileWasTooBig = true;
-                        return View(ingredient);
-                    }                        
-                    else
-                        ingredient.CloudFileId = UploadedCloudFileId;
-                }
-                //Save Ingredient
-                db.Ingredients.Add(ingredient);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(ingredient);
-        }       
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)            
                 db.Dispose();            
             base.Dispose(disposing);
         }
+
 
         public JsonResult AjaxGetIngredients(int pageIndex, int pageSize)
         {
@@ -86,8 +31,7 @@ namespace OpenSourceCooking.Controllers.StandardControllers
                                                    CreateDate = i.CreateDate
                                                }).Skip(pageIndex * pageSize).Take(pageSize).ToList();
             return Json(Ingredients, JsonRequestBehavior.AllowGet);
-        }
-        
+        }        
         [HttpPost]
         public JsonResult AjaxIngredientsAutoComplete(string prefix)
         {
